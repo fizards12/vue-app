252cede096b7bac2d322033cd1cf20a706ccdbf1
fizards12 <mahmoudsameh734@gmail.com>
2024-07-11 15:23:16 +0300

    first commit

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>About Page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About'
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles here */
+  </style>
+  
